d0466e3798fa40c41db43164f210db5f35c23c26
dumoulma <mathieu.dumoulin@gmail.com>
2013-08-05 12:24:34 -0400

    added ScoreWithPerceptron.

---
+++ src/main/java/com/fujitsu/ca/fic/caissepop/evaluation/kdd1999/ScoreWithPerceptron.java
@@ -0,0 +1,86 @@
+package com.fujitsu.ca.fic.caissepop.evaluation.kdd1999;
+
+import java.io.IOException;
+
+import org.apache.commons.logging.Log;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.mahout.math.NamedVector;
+import org.apache.mahout.math.SequentialAccessSparseVector;
+import org.apache.mahout.math.Vector;
+import org.apache.pig.EvalFunc;
+import org.apache.pig.backend.executionengine.ExecException;
+import org.apache.pig.data.DataType;
+import org.apache.pig.data.Tuple;
+import org.apache.pig.impl.util.UDFContext;
+
+import com.fujitsu.ca.fic.classifier.perceptron.Perceptron;
+
+public class ScoreWithPerceptron extends EvalFunc<Double> {
+    private Perceptron perceptron = null;
+    private static final int FIELDS = 42;
+    private static final int FEATURES = 41;
+    private static final int FIRST_FEATURE_INDEX = 1;
+    private static final int LABEL_INDEX = 0;
+    private final Log LOG = getLogger();
+
+    @Override
+    public Double exec(Tuple input) throws IOException {
+        if (perceptron == null)
+            loadPerceptronFromFS();
+
+        if (input == null || input.size() != FIELDS) {
+            LOG.error("Input does not have expected number of fields!");
+            return null;
+        }
+
+        NamedVector example = tupleToNamedVector(input);
+        return perceptron.classifyScalar(example);
+    }
+
+    private NamedVector tupleToNamedVector(Tuple input) throws ExecException {
+        double[] featuresDouble = new double[FEATURES];
+        int label;
+        try {
+            if (input.getType(LABEL_INDEX) == DataType.INTEGER) {
+                label = (Integer) input.get(LABEL_INDEX);
+            } else if (input.getType(LABEL_INDEX) == DataType.DOUBLE) {
+                label = (int) (double) (Double) input.get(LABEL_INDEX);
+            } else {
+                throw new ExecException("Unexpected format for label field: " + input.get(LABEL_INDEX));
+            }
+
+            int j = 0;
+            for (int i = FIRST_FEATURE_INDEX; i < FIELDS; i++) {
+                if (input.getType(i) == DataType.INTEGER) {
+                    featuresDouble[j++] = (Integer) input.get(i);
+                } else if (input.getType(i) == DataType.DOUBLE) {
+                    featuresDouble[j++] = (Double) input.get(i);
+                } else {
+                    throw new ExecException("Unexpected format for field: " + i + "=" + input.get(i));
+                }
+            }
+        } catch (Exception e) {
+            LOG.error(this, e);
+            return null;
+        }
+
+        Vector featureVector = new SequentialAccessSparseVector(FEATURES);
+        featureVector.assign(featuresDouble);
+
+        return new NamedVector(featureVector, Integer.toString(label));
+    }
+
+    private void loadPerceptronFromFS() throws IOException {
+        perceptron = new Perceptron(UDFContext.getUDFContext().getJobConf());
+    }
+
+    // FOR TESTING PURPOSES
+    void setPerceptron(Perceptron perceptronMock) {
+        perceptron = perceptronMock;
+    }
+
+    // FOR TESTING PURPOSES
+    void setConf(Configuration conf) {
+        UDFContext.getUDFContext().addJobConf(conf);
+    }
+}
---
+++ src/test/java/com/fujitsu/ca/fic/caissepop/evaluation/kdd1999/ScoreWithPerceptronTest.java
@@ -0,0 +1,107 @@
+package com.fujitsu.ca.fic.caissepop.evaluation.kdd1999;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.mahout.math.Vector;
+import org.apache.pig.data.Tuple;
+import org.apache.pig.data.TupleFactory;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import com.fujitsu.ca.fic.classifier.perceptron.Perceptron;
+import com.google.common.base.Function;
+import com.google.common.collect.Lists;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import static org.hamcrest.number.OrderingComparison.greaterThan;
+import static org.hamcrest.number.OrderingComparison.greaterThanOrEqualTo;
+
+import static org.mockito.Matchers.anyObject;
+
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ScoreWithPerceptronTest {
+    private static final int FIELDS = 42;
+    private final String goodLineOfData = "1,0,1,12,1,146,38132,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,2,20,0.0,0.0,0.0,0.0,1.0,0.0,0.2,255.0,255.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0";
+    private final String lineOfDataWithIncorrectNumberOfFields = "0,3,22,1,1032,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,511,511,0.0,0.0,0.0,0.0,1.0,0.0,0.0,255.0,255.0,1.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0";
+
+    private static final Configuration conf = new Configuration();
+
+    @Mock
+    Perceptron perceptronMock;
+
+    @BeforeClass
+    public static void beforeClass() {
+        conf.set("perceptron.model.path", "model/perceptron.model");
+    }
+
+    @Test
+    public void testExecWithMockPerceptron() throws Exception {
+        TupleFactory tupleFactory = TupleFactory.getInstance();
+        ScoreWithPerceptron scorer = new ScoreWithPerceptron();
+
+        when(perceptronMock.classifyScalar((Vector) anyObject())).thenReturn(new Double(1.0));
+        scorer.setPerceptron(perceptronMock);
+        Tuple testTuple = tupleFactory.newTuple(toDoubleList(goodLineOfData));
+
+        assertThat(testTuple.size(), equalTo(FIELDS));
+
+        Double confidenceScore = scorer.exec(testTuple);
+
+        assertThat(confidenceScore, greaterThan(0.0));
+    }
+
+    @Test
+    public void testExecWithPerceptronReturnsDouble() throws IOException {
+        TupleFactory tupleFactory = TupleFactory.getInstance();
+        ScoreWithPerceptron scorer = new ScoreWithPerceptron();
+        scorer.setConf(conf);
+
+        List<Double> list = toDoubleList(goodLineOfData);
+        Tuple testTuple = tupleFactory.newTuple(list);
+        assertThat(testTuple.size(), equalTo(FIELDS));
+
+        Double confidenceScore = scorer.exec(testTuple);
+
+        assertThat(confidenceScore, greaterThanOrEqualTo(new Double(0.0)));
+    }
+
+    @Test
+    public void testWithIncorrectlyFormattedDataShouldThrow() throws IOException {
+        TupleFactory tupleFactory = TupleFactory.getInstance();
+        ScoreWithPerceptron scorer = new ScoreWithPerceptron();
+        scorer.setConf(conf);
+
+        Tuple testTuple = tupleFactory.newTuple(toDoubleList(lineOfDataWithIncorrectNumberOfFields));
+        scorer.exec(testTuple);
+    }
+
+    @Test
+    public void testPerceptronModelLoadsFromConf() throws IOException {
+        Perceptron p = new Perceptron(conf);
+        System.out.println(p);
+
+        assertThat(p.toString().contains("alpha lengthSquared=1000.0"), is(true));
+    }
+
+    private List<Double> toDoubleList(String line) {
+        String[] tokens = line.split(",");
+        return Lists.transform(Arrays.asList(tokens), new Function<String, Double>() {
+            @Override
+            public Double apply(String token) {
+                return Double.parseDouble(token);
+            }
+        });
+    }
+}
