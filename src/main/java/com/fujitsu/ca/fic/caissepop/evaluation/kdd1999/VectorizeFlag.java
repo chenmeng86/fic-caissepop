17493aeac03fc7e004f6960ebf15cd056b8766e7
dumoulma <mathieu.dumoulin@gmail.com>
2013-08-26 13:13:28 -0400

    fixed error in test execution because hamcrest needs to be declared in dependencies before junit.

--- src/main/java/com/fujitsu/ca/fic/caissepop/evaluation/kdd1999/VectorizeFlag.java
+++ src/main/java/com/fujitsu/ca/fic/caissepop/evaluation/kdd1999/VectorizeFlag.java
@@ -12,33 +12,34 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class VectorizeFlag extends EvalFunc<Integer> {
-    private final Logger log = LoggerFactory.getLogger(VectorizeFlag.class);
-    private static final String[] FLAG_NAMES = {"SF", "S0", "S1", "S2", "S3", "OTH", "REJ", "RSTO", "RSTR", "RSTOS0", "SH", "RSTRH", "SHR",
-            "RESTR_FLAG"};
+	private final Logger log = LoggerFactory.getLogger(VectorizeFlag.class);
+	private static final String[] FLAG_NAMES = { "SF", "S0", "S1", "S2", "S3",
+			"OTH", "REJ", "RSTO", "RSTR", "RSTOS0", "SH", "RSTRH", "SHR",
+			"RESTR_FLAG" };
 
-    private final Map<String, Integer> flags = new HashMap<String, Integer>();
+	private static final Map<String, Integer> flags = new HashMap<String, Integer>();
+	{
+		for (int i = 0; i < FLAG_NAMES.length; i++) {
+			flags.put(FLAG_NAMES[i], i);
+		}
+	}
 
-    public VectorizeFlag() {
-        for (int i = 0; i < FLAG_NAMES.length; i++) {
-            flags.put(FLAG_NAMES[i], i);
-        }
-    }
+	@Override
+	public Integer exec(Tuple input) throws IOException {
+		if (input == null) {
+			log.warn("input is null!");
+			return null;
+		}
+		if (input.size() != 1 || input.isNull(0)) {
+			log.warn("Unexpected input Tuple size: " + input.size());
+			return null;
+		}
 
-    @Override
-    public Integer exec(Tuple input) throws IOException {
-        if (input == null) {
-            log.warn("input is null!");
-            return null;
-        }
-        if (input.size() != 1 || input.isNull(0)) {
-            log.warn("Unexpected input Tuple size: " + input.size());
-            return null;
-        }
-
-        String flagString = (String) input.get(0);
-        if (!flags.containsKey(flagString)) {
-            throw new ExecException("Flag Type unknown value: " + flagString, PigException.INPUT);
-        }
-        return flags.get(flagString);
-    }
+		String flagString = (String) input.get(0);
+		if (!flags.containsKey(flagString)) {
+			throw new ExecException("Flag Type unknown value: " + flagString,
+					PigException.INPUT);
+		}
+		return flags.get(flagString);
+	}
 }
--- src/test/java/com/fujitsu/ca/fic/caissepop/evaluation/kdd1999/VectorizeFlagTest.java
+++ src/test/java/com/fujitsu/ca/fic/caissepop/evaluation/kdd1999/VectorizeFlagTest.java
@@ -7,18 +7,42 @@ import org.apache.pig.data.TupleFactory;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.equalTo;
-
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class VectorizeFlagTest {
+	private VectorizeFlag vectorizer = new VectorizeFlag();
+	
     @Test
-    public void givenTokenShouldReturnCorrectNumber() throws IOException {
-        VectorizeFlag vectorizer = new VectorizeFlag();
+    public void givenTokenSFShouldReturnZero() throws IOException {
         Tuple input = TupleFactory.getInstance().newTuple();
         input.append("SF");
 
-        int value = vectorizer.exec(input);
-        assertThat(value, equalTo(1));
+        Integer value = vectorizer.exec(input);
+        assertThat(value, equalTo(0));
     }
-
+    
+    @Test
+    public void givenNullTokenShouldReturnNull() throws IOException {
+        Integer value = vectorizer.exec(null);
+        assertThat(value, is(nullValue()));
+    }
+    
+    @Test
+    public void givenTupleWithIncorrectSizeShouldReturnNull() throws IOException {
+    	Tuple input = TupleFactory.getInstance().newTuple();
+        input.append("1");
+        input.append("2");
+        Integer value = vectorizer.exec(input);
+        assertThat(value, is(nullValue()));
+    }    
+    
+    @Test
+    public void givenTupleWithNullContentShouldReturnNull() throws IOException {
+    	Tuple input = TupleFactory.getInstance().newTuple();
+        input.append(null);
+        Integer value = vectorizer.exec(input);
+        assertThat(value, is(nullValue()));
+    } 
 }
